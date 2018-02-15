f1047194de82cfee94c506a920dd47c3876d7299
brewmanandi <brewman@gmx.at>
2018-02-15 16:13:06 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,12 +84,17 @@ app.get('/counter', function (req, res) {
     res.send(counter.toString());
 });
 
+var names = [];
+app.get('/name/:currentName', function (req, res) {
+    names.push(currentName);
+    res.send(JSON.stringify(names));
+});
+
+
 
 app.get('/:articleName', function (req, res) {
     res.send(createTemplate(articles[req.params.articleName]))
 });
-
-
 
 
 
