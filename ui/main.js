5aed99d66d68be16075847471e8173185f6fc047
brewmanandi <brewman@gmx.at>
2018-02-15 15:32:31 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,11 +7,11 @@ element.innerHTML = "New value"
 // move image
 var imageElement = document.getElementById('image')
 imageElement.onclick= function () {
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 10);
     
 }
 var marginLeft = 0;
 function moveRight() {
-    marginLeft += 10;
+    marginLeft += 1;
     image.style.marginLeft=marginLeft + 'px';
 }
