63a5788f2198e3e82e954f7c1e2be5099456f41b
brewmanandi <brewman@gmx.at>
2018-02-15 15:42:02 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var counter;
+var counter = 0;
 app.get('/counter', function (req, res) {
     counter++;
     res.send(counter.toString());
