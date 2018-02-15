63d2169f88480d73c5241268d9e957f03e1a2a60
brewmanandi <brewman@gmx.at>
2018-02-15 15:28:30 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 // change text
 var element = document.getElementById('hi')
 element.innerHTML = "New value"
+
+// move image
+var imageElement = document.getElementById('image')
+imageElement.onclick= function () {
+    image.style.marginLeft='100px';
+}
