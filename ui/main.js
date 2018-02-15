f51a37eb552f04a49d00f06e098614400c57eee0
brewmanandi <brewman@gmx.at>
2018-02-15 15:57:33 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ button.onclick= function () {
             if (request.status === 200) {
                 var counter = request.responseText
                 var span = document.getElementById("count");
-                span.innerHttml = counter.toString();
+                span.innerHtml = counter.toString();
             }
         }    
     
