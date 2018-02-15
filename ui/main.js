db8c93cddba9cc4474cf3723031a3a585098d16a
brewmanandi <brewman@gmx.at>
2018-02-15 16:13:11 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,12 +23,23 @@ var name = nameInput.value;
 var submitButton = document.getElementById("submit_btn");
 submitButton.onclick = function() {
     // make request with name, capture list of names
+    var request = new XMLHttpRequest();
+     request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var names = JSON.parse(request.responseText);
+                var list = "";
+                for (var i=0; i<names.length; i++) {
+                            list+="<li>"+names[i]+"</li>";
+                }
+                var ul = document.getElementById("namelist");
+                ul.innerHTML = list;
+            }
+        }
+    };
+
+    request.open("GET," "http://andreasbraumann.imad.hasura-app.io/name", "true");
+    request.send(null);
     
-    var names = ["name1", "name2", "name3"];
-    var list = "";
-    for (var i=0; i<names.length; i++) {
-        list+="<li>"+names[i]+"</li>";
     }
-    var ul = document.getElementById("namelist");
-    ul.innerHTML = list;
 }
