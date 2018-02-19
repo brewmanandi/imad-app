67fd1fc406affcd1d628f1e5cbfe839a7527ca62
brewmanandi <brewman@gmx.at>
2018-02-19 13:09:03 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'andreasbraumann',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB_PASSWORD
+    password: 'db-andreasbraumann-93828'
 }
 var app = express();
 app.use(morgan('combined'));
