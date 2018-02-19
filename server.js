1710681b23f1bff30ab9d06c8f5c2a4acae0c260
brewmanandi <brewman@gmx.at>
2018-02-19 14:24:33 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,7 +113,22 @@ app.get('/name/:currentName', function (req, res) {
 
 
 
-app.get('/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
+    var articleName = req.params.articleName;
+    
+    pool.query("SELECT * FROM articles WHERE title= '"+articleName+"'", function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            if (result.rows.length ===0) {
+                res.status(404).send('Article not found.');
+            } else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    })
+    
     res.send(createTemplate(articles[req.params.articleName]))
 });
 
