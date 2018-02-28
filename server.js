4fb3aadb8e411349281c9c3caabf809463784d4a
brewmanandi <brewman@gmx.at>
2018-02-28 10:20:23 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/hash/:input', function(req, res) {
 });
 
 function hash(input, salt) {
-    var hashed = pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex')
 }
 
