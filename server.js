f72e1e085bd0fbd16da03e84aac96b136a8d322d
brewmanandi <brewman@gmx.at>
2018-02-28 10:44:07 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,30 @@ app.post('/create-user', function (req, res) {
    });
 });
 
+app.post('/login', function(req, res) {
+    pool.query('SELECT " FROM "user"WHERE username=$1)', [username], function (err, result) {
+       if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            if (result.rows.length===0) {
+                res.send(403).send('username/password is invalid');
+            } else {
+                // match pw
+                var dbPassword = result.rows[0].password
+                var salt = dbPassword.split('$')[2];
+                var hashedPassword = hash(password, salt); // create hash based on submitted pw and original salt
+                if (hashedPassword === dbString) {
+                    res.send('User logged in.');
+                } else {
+                    res.send(403).send("username/password is invalid");
+                }
+            }
+            
+            res.send("User successfully created: " + username);
+        }
+   });
+});
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
