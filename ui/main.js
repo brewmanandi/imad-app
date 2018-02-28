8229cb517843e7af1f16871e8913cfd2d7db6ebd
brewmanandi <brewman@gmx.at>
2018-02-28 10:56:58 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,8 @@ submitButton.onclick = function() {
     var password = document.getElementById('password').value;
     console.log(username);
     console.log(password);
-    request.open("POST", "http://andreasbraumann.imad.hasura-app.io/login, "true");
+    request.open("POST", "http://andreasbraumann.imad.hasura-app.io/login", "true");
+    request.setRequestHeader('Content-Type', 'application: json');
     request.send(JSON.stringify({username: username, password: password}));
     
 };
