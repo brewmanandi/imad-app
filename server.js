bbe1aff1cc9fbafe8623abbbe9492c4ffcd79a02
brewmanandi <brewman@gmx.at>
2018-03-06 16:19:59 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -198,8 +198,7 @@ app.get('/get-articles', function (req, res) {
             if (result.rows.length ===0) {
                 res.status(404).send('Article not found.');
             } else {
-                var articleData = result.rows[0];
-                res.send(JSON.stringify(articleData));
+                res.send(JSON.stringify(result));
             }
         }
     })
